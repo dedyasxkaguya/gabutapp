12e92f63994f5677943de99b862be23555e5bb3a
dedyasxkaguya <dedysgithub@gmail.com>
2025-09-27 14:59:57 +0700

    third commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,32 @@
-fetch('https://www.demonslayer-api.com/api/v1/characters?id=30')
-.then(response=>response.json())
-.then(data=>console.table(data.content))
+const res = document.getElementById("resultBox")
+const search = document.getElementById("search")
+fetch('../data/character.json')
+    .then(response => response.json())
+    .then(data => {
+        data.characters.forEach((c) => {
+            res.innerHTML += `
+          <div class="result flex ${c.gender} p-3 rounded-full items-center">
+                <div class="imgBox${c.gender} p-3 rounded-full">
+                    <img src="${c.image}" alt="">
+                </div>
+                <span class="name mx-4 font-semibold">${c.name}</span>
+                <span class="id bg-neutral-800 text-neutral-50 rounded-full flex justify-center items-center p-2">
+                ${c.id}</span>
+            <span class="arc mx-4 font-light">ARC ${c.arc_id}</span>
+        </div>
+        `
+        })
+    })
+
+search.addEventListener("keyup", (e) => {
+    let keyValue = e.target.value
+    console.log(keyValue)
+    document.querySelectorAll(".result").forEach((el) => {
+        console.log(el.innerText.trim())
+        if (el.textContent.trim().toLowerCase().includes(keyValue)) {
+            el.style.display = 'flex'
+        } else {
+            el.style.display = 'none'
+        }
+    })
+})
--- test.js
+++ test.js
@@ -5,68 +5,19 @@ const search = document.getElementById("search")
 let isLoaded = false
 let max = 9
 let pendingMsg = `Wait A Second`
-let nime = []
-let nime0 = []
-let nime1 = []
 let apiUrl
 
-
-const takeData = (array) => {
-    if (array == '') {
-        console.log('pending')
-    } else {
-
-        let srtArray = array.sort((a, b) => {
-            // b.score - a.score ensures descending numeric order
-            return a.id - b.id;
-        });
-        srtArray.forEach((c) => {
-            // console.table(c)
-            test.innerHTML += `
-        <div id="${c.id}" class="flex">
-        ${c.id} ${c.name}
-        </div>
+fetch("./data/character.json")
+.then(response=>response.json())
+.then(data=>{
+    console.table(data.characters)
+    data.characters.forEach((c)=>{
+        test.innerHTML+=`
+        <div>${(c.id)+1} ${c.name}</div>
         `
-        })
-    }
+    })
+})
 
-}
-
-const getData = (link) => {
-    fetch(link)
-        .then(response => response.json())
-        .then(data => {
-            data.content.forEach((c) => {
-                nime0.push({
-                    id: c.id,
-                    name: c.name,
-                    age: c.age,
-                    gender: c.gender,
-                    race: c.race,
-                    aff_id: c.affiliation_id,
-                    arc_id: c.arc_id
-                })
-                nime1.push({
-                    description: c.description,
-                    image: c.img,
-                    quote: c.quote
-                })
-                nime.push(c)
-            })
-        })
-}
-for (let i = 1; i <= max; i++) {
-    if (i == 1) {
-        getData(`https://www.demonslayer-api.com/api/v1/characters?`)
-    } else {
-        getData(`https://www.demonslayer-api.com/api/v1/characters?page=${i}`)
-    }
-}
-setInterval(() => {
-    while (test.innerHTML == '') {
-        takeData(nime)
-    }
-}, 1000)
 search.addEventListener("keyup", (e) => {
     let keyValue = e.target.value
     console.log(keyValue)
