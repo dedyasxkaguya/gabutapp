c60b216ff3a1ee39316fdc3683a5e4f5e071ca76
dedyasxkaguya <dedysgithub@gmail.com>
2025-09-27 22:50:56 +0700

    third commit

--- script/script.js
+++ script/script.js
@@ -1,23 +1,35 @@
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".skeleton").forEach((el) => {
+        el.classList.remove("skeleton")
+    })
+})
+
 const res = document.getElementById("resultBox")
 const search = document.getElementById("search")
-fetch('../data/character.json')
-    .then(response => response.json())
-    .then(data => {
-        data.characters.forEach((c) => {
-            res.innerHTML += `
-          <div class="result flex ${c.gender} p-3 rounded-full items-center">
-                <div class="imgBox${c.gender} p-3 rounded-full">
-                    <img src="${c.image}" alt="">
-                </div>
-                <span class="name mx-4 font-semibold">${c.name}</span>
-                <span class="id bg-neutral-800 text-neutral-50 rounded-full flex justify-center items-center p-2">
-                ${c.id}</span>
-            <span class="arc mx-4 font-light">ARC ${c.arc_id}</span>
-        </div>
-        `
-        })
-    })
+const getData = async () => {
 
+    fetch('./data/character.json')
+        .then(response => response.json())
+        .then(data => {
+            data.characters.forEach((c) => {
+                res.innerHTML += `
+            <div class="result ${c.gender} p-3 rounded-full items-center shadow">
+            <div class="imgBox${c.gender} p-3 rounded-full skeleton">
+            <img src="${c.image}" alt="">
+            </div>
+            <span class="name mx-4 font-semibold skeleton">${c.name}</span>
+            <span class="id bg-neutral-800 text-neutral-50 rounded-full flex justify-center items-center p-2 skeleton">
+            ${c.id}</span>
+            <span class="arc mx-4 font-light arc skeleton">ARC ${c.arc_id}</span>
+            </div>
+            `
+            })
+            document.querySelectorAll(".skeleton").forEach((el) => {
+                el.classList.remove("skeleton")
+            })
+        })
+}
+getData()
 search.addEventListener("keyup", (e) => {
     let keyValue = e.target.value
     console.log(keyValue)
